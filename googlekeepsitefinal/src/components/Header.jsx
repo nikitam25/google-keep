6cb1d332b81dee233d79834dc946de5af7c16586
nikitaco <82123863+nikitaco@users.noreply.github.com>
2021-04-10 21:18:47 -0400

    Add files via upload

---
+++ googlekeepsitefinal/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+        <img
+          src="https://www.gstatic.com/images/branding/product/1x/keep_2020q4_48dp.png"
+          alt="new"/>
+      <h1>
+        Keep
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
